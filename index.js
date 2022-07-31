67f671d2a9d23498f26492dfcf2ceb6793533cf9
Davide Arcinotti <mastrobardo@gmail.com>
2022-07-31 22:16:01 +0200

    add gzip

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path= require('path');
+const compression = require('compression')
 const cors = require('cors');
 const server = express();
-server.use(cors())
+server.use(compression);
+server.use(cors());
 
 server.get('/games', (req, res) => {
     res.sendFile(path.join(__dirname + '/data/original.json'));
-})
+});
 
 const port = 8000;
 server.listen(process.env.PORT || port);
